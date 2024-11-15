790a25428681131669ead23ccd0822288dcc1a32
DarjaDaisY <darja.veschikovaayandex.ru@gmail.com>
2024-11-15 19:30:13 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict = {'Leon': 21_10_1989, 'Victoria': 11_01_2003, 'Max': 27_10_2000}
+print(my_dict)
+print(my_dict['Max'])
+my_dict.update({'Din': 25_06_1995, 'Maria': 2_12_2002})
+a = my_dict.pop('Max')
+print(my_dict)
+print(a)
+set_={1, 1, 2, 3, 4.3, True, 'Mam'}
+set_.add(7)
+set_.add('Dad')
+set_.remove(1)
+print(set_)
